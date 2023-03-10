ba94ccd614619483d0285bdf68ac249a19acae24
renaud dor <renaud.dor@gmail.com>
2023-03-10 11:31:03 +0100

    Start to design requests/response and error handling for POST /installations endpoint

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,17 +11,20 @@ const company = ref({
   name: '',
   siren: ''
 })
+const company_error = ref('')
 
 const customer = ref({
   name: '',
   email: '',
   phone: ''
 })
+const customer_errors = ref({}) // TODO: define type
 
 const installation = ref({
-  address: '',
+  address1: '',
   date: '',
 })
+const installation_errors = ref({}) // TODO: define type
 
 const panels = ref<panel[]>([
   {
@@ -42,6 +45,35 @@ const addPanel = () => {
 const removePanel = (index: number) => {
   panels.value.splice(index, 1)
 }
+
+const submitForm = async () => {
+  const payload = JSON.stringify({
+    company: company.value,
+    customer: customer.value,
+    installation: installation.value,
+    panels: panels.value
+  })
+
+  fetch('http://localhost:3000/installations', { // TODO: don't hardcode this url
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: payload
+  }).then((response) => response.json())
+    .then((data) => {
+      if (data.status === 200) {
+        console.log('success')
+      } else {
+        processErrors(data.errors)
+      }
+    })
+    .catch(() => console.log('Something went wrong')) // TODO: trigger flash. Why do we get here for 4xx response?
+}
+
+const processErrors = (errors) => {
+  company_error.value = errors.company
+  customer_errors.value = errors.customer
+  installation_errors.value = errors.installation
+}
 </script>
 
 <template>
@@ -49,6 +81,9 @@ const removePanel = (index: number) => {
     <h2>Company Infos</h2>
     <input v-model="company.name" placeholder="Name" />
     <input v-model="company.siren" placeholder="Siren" />
+    <div>
+      {{ company_error }}
+    </div>
   </div>
 
   <div>
@@ -56,12 +91,24 @@ const removePanel = (index: number) => {
     <input v-model="customer.name" placeholder="Name" />
     <input v-model="customer.email" placeholder="Email" />
     <input v-model="customer.phone" placeholder="Phone" />
+    <div>
+      <ul>
+        <li v-for="(value, key) in customer_errors" :key="key">
+          {{ key }}: {{ value.join(', ') }}
+        </li>
+      </ul>
+    </div>
   </div>
 
   <div>
     <h2>Installation Details</h2>
-    <input v-model="installation.address" placeholder="Address" />
-    <input v-model="installation.date" placeholder="Date" />
+    <input v-model="installation.address1" placeholder="Address" />
+    <input v-model="installation.date" placeholder="Date" /> <!-- TODO: set as HTML5 date input-->
+    <ul> <!-- TODO: not very DRY... -->
+      <li v-for="(value, key) in installation_errors" :key="key">
+        {{ key }}: {{ value.join(', ') }}
+      </li>
+    </ul>
   </div>
 
   <div>
@@ -77,6 +124,10 @@ const removePanel = (index: number) => {
     </div>
     <button @click="addPanel">Add Panel</button>
   </div>
+
+
+  <br />
+  <button type="submit" @click="submitForm">OK!</button>
 </template>
 
 <style scoped>
--- server/app/controllers/installations_controller.rb
+++ server/app/controllers/installations_controller.rb
@@ -2,6 +2,42 @@
 
 class InstallationsController < ApplicationController
   def create
-    binding.pry
+    errors = {}
+
+    company = Company.find_by(company_params)
+    errors[:company] = "Couldn't find Company, please review Name and SIREN" unless company
+
+    customer = Customer.find_or_initialize_by(customer_params)
+    errors[:customer] = customer.errors.messages if customer.new_record? && !customer.save
+
+    installation = Installation.new(installation_params)
+    installation.company = company
+    installation.customer = customer
+    errors[:installation] = installation.errors.messages.except(:company, :customer) unless installation.save
+
+    if errors.any?
+      payload = {
+        errors: errors,
+        status: 422
+      }
+      render json: payload, status: :unprocessable_entity
+    else
+      payload = { status: 200 }
+      render json: payload, status: :ok
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :siren)
+  end
+
+  def customer_params
+    params.require(:customer).permit(:name, :email, :phone)
+  end
+
+  def installation_params
+    params.require(:installation).permit(:address1, :date)
   end
 end
