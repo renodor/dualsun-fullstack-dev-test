dd45dbe02fbbee9bc119799bec3d4b3d854bc0a2
renaud dor <renaud.dor@gmail.com>
2023-03-10 08:57:18 +0100

    Delete angular code and setp vue app

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,6 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import './assets/main.css'
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+createApp(App).mount('#app')
